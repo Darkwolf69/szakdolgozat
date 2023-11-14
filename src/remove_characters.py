e2399bf7ebe73de1597b574507227656c10b0930
Darkwolf69 <69400316+Darkwolf69@users.noreply.github.com>
2023-11-14 02:10:32 +0100

    Update remove_characters.py

--- src/remove_characters.py
+++ src/remove_characters.py
@@ -8,9 +8,9 @@ Created on Mon Nov 13 02:13:27 2023
 import re
 
 def remove_unnecessary_chars(data):
-    
+
     # remove unnecessary characters from text data
-    data = re.sub('[,.:;!?"()]','',data)
+    data = re.sub('[,.:;!?"()–—]','',data)
     data = data.replace(' – ',' ')
-    
+
     return data
